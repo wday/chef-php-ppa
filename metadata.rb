d5f6456dfc1152f138a4ea93de76de4a0fda83fe
Rob Johnson <robert@turbine.com>
2014-03-10 18:50:16 -0400

    Versioning to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,6 @@ license           "GPL v2 - All rights reserved"
 description       "Chef Cookbook for setting up a PHP web server using a PPA."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports          "ubuntu"
+version          "1.0.0"
 
 depends "apt"
