530fc838d5285cb6d83520b3ed121a4a80def811
Rob Johnson <robert@turbine.com>
2014-03-13 16:03:37 -0400

    updating deprecated minitest to fix deprecated method

--- test/integration/default/minitest/test_default.rb
+++ test/integration/default/minitest/test_default.rb
@@ -2,6 +2,6 @@ require 'minitest/autorun'
 
 describe 'default' do
   it 'has created ondrej-php-precise.list' do
-    assert File.exists?('/etc/apt/sources.list.d/ondrej-php-precise.list')
+    assert File.exist?('/etc/apt/sources.list.d/ondrej-php-precise.list')
   end
 end
