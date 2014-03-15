2c4ad5ad1fb7af65d5262854b4059b3b67d3911e
Rob Johnson <robert@turbine.com>
2014-03-14 20:27:24 -0400

    test kitchen .. for ec2 .. I hope

---
+++ travis_ec2_encrypt.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# travis_ec2_encrypt.ssh
+#
+# Encrypts private SSH keys for use in .travis.yml.
+#
+# Based on the following gists:
+#   https://gist.github.com/lukewpatterson/4242707
+#   https://gist.github.com/davestern/9377538
+#
+# Also see the travis documentation:
+#   http://docs.travis-ci.com/user/build-configuration/#Secure-environment-variables
+ 
+# Usage:
+# Go to your git project:
+#   cd my_project
+# Then run:
+#   ./travis_ec2_encrypt.sh ~/.ssh/my_private_key
+ 
+SSH_KEY_PATH=$1
+ 
+base64 --break 64 ${SSH_KEY_PATH} > ${SSH_KEY_PATH}_base64
+ 
+if [[ ! $(which travis) ]]
+then
+  gem install travis
+fi
+ 
+travis login --pro --auto
+ 
+for l in $(cat ${SSH_KEY_PATH}_base64);
+do
+  LINE=$(grep -n $l ${SSH_KEY_PATH}_base64 | cut -d: -f1);
+  echo "- secure: $(travis encrypt \"EC2_KEY_CHUNK_$LINE=$l\")";
+done
+
