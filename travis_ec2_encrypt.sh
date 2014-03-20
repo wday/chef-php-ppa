5fd10b50231c3adfaa9ed2bc6f75724a0a45d456
Robert Johnson <robj@gridiron-guru.com>
2014-03-20 00:12:37 -0400

    Updating travis.yml to see if i can get the env vars correct

--- travis_ec2_encrypt.sh
+++ travis_ec2_encrypt.sh
@@ -42,11 +42,18 @@ travis login --pro --auto
 for l in $(cat ${SSH_KEY_PATH}_base64);
 do
   LINE=$(grep -n $l ${SSH_KEY_PATH}_base64 | cut -d: -f1);
-  echo "    - secure: $(travis encrypt \"EC2_KEY_CHUNK_$LINE=$l\")";
+  travis encrypt EC2_KEY_CHUNK_$LINE=$l --add
+#  echo "    - secure: $(travis encrypt \"EC2_KEY_CHUNK_$LINE=$l\")";
 done
 
-echo "    - secure: $(travis encrypt \"AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID\")";
-echo "    - secure: $(travis encrypt \"AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY\")";
-echo "    - secure: $(travis encrypt \"AWS_KEYPAIR_NAME=$AWS_KEYPAIR_NAME\")";
-echo "    - secure: $(travis encrypt \"AWS_USERNAME=$AWS_USERNAME\")";
-echo "    - secure: $(travis encrypt \"EC2_SSH_KEY_PATH=$EC2_SSH_KEY_PATH\")";
+travis encrypt AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --add
+travis encrypt AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --add
+travis encrypt AWS_KEYPAIR_NAME=$AWS_KEYPAIR_NAME --add
+travis encrypt AWS_USERNAME=$AWS_USERNAME --add
+travis encrypt EC2_SSH_KEY_PATH=$EC2_SSH_KEY_PATH --add
+
+# echo "    - secure: $(travis encrypt AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID\")";
+# echo "    - secure: $(travis encrypt AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY\")";
+# echo "    - secure: $(travis encrypt AWS_KEYPAIR_NAME=$AWS_KEYPAIR_NAME\")";
+# echo "    - secure: $(travis encrypt AWS_USERNAME=$AWS_USERNAME\")";
+# echo "    - secure: $(travis encrypt EC2_SSH_KEY_PATH=$EC2_SSH_KEY_PATH)";
